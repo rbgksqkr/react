39fc5f2f8ed0d5b42567ea0c959d825ce001b919
ghenmaru <rbgks1937@naver.com>
2022-12-20 18:00:24 +0900

    feat: navbar component 구현

---
+++ shopping/src/components/Navbar.js
@@ -0,0 +1,62 @@
+import {
+  AiOutlineShoppingCart,
+  AiOutlineSearch,
+  AiOutlineUnorderedList,
+} from "react-icons/ai";
+import { MdPersonOutline } from "react-icons/md";
+import { useState } from "react";
+import "./Navbar.scss";
+import coupang from "../images/coupang-logo.png";
+
+const Navbar = () => {
+  const [value, setValue] = useState("");
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setValue("");
+    console.log(value);
+  };
+  return (
+    <div className="navbar">
+      <div className="category">
+        <div>
+          <div className="category-image">
+            <AiOutlineUnorderedList />
+          </div>
+          <span>카테고리</span>
+        </div>
+      </div>
+      <div className="home">
+        <img src={coupang} alt="home" />
+      </div>
+      <form className="search" onSubmit={onSubmit}>
+        <input
+          className="search-input"
+          placeholder="찾고 싶은 상품을 검색하세요!"
+          onChange={onChange}
+          value={value}
+        />
+        <button>
+          <AiOutlineSearch />
+        </button>
+      </form>
+
+      <div className="mypage">
+        <div className="mypage-image">
+          <MdPersonOutline />
+        </div>
+        <span>마이쿠팡</span>
+      </div>
+      <div className="basket">
+        <div className="basket-image">
+          <AiOutlineShoppingCart />
+        </div>
+        <span>장바구니</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
