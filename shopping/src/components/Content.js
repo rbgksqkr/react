383b29be0e4f8d530d8538ed14958b437226ca0d
ghenmaru <rbgks1937@naver.com>
2023-01-18 17:42:24 +0900

    feat: 추천상품 리스트 추가

--- shopping/src/components/Content.js
+++ shopping/src/components/Content.js
@@ -1,6 +1,7 @@
 import styles from "./Content.module.scss";
 import { useState } from "react";
 import ContentNewItem from "./ContentNewItem";
+import ContentRecoItem from "./ContentRecoItem";
 
 const Content = () => {
   const [items, setItems] = useState([
@@ -36,36 +37,66 @@ const Content = () => {
     },
   ]);
 
+  const [recommendItems, setRecommendItems] = useState([
+    {
+      id: 1,
+      name: "두피케어 샴푸",
+      price: "11,340원",
+      src: "images/1.jpg",
+    },
+    {
+      id: 2,
+      name: "두피케어 샴푸",
+      price: "11,340원",
+      src: "images/1.jpg",
+    },
+    {
+      id: 3,
+      name: "두피케어 샴푸",
+      price: "11,340원",
+      src: "images/1.jpg",
+    },
+    {
+      id: 4,
+      name: "두피케어 샴푸",
+      price: "11,340원",
+      src: "images/1.jpg",
+    },
+    {
+      id: 5,
+      name: "두피케어 샴푸",
+      price: "11,340원",
+      src: "images/1.jpg",
+    },
+  ]);
+
   return (
     <div>
-      {/* 신상품으로 분리 */}
-
-      <span style={{ fontSize: "20px" }}>오늘의 발견</span>
-      <span style={{ fontSize: "13px" }}>
-        | 오늘 쿠팡이 엄선한 가장 HOT한 신상품 !
-      </span>
-      <br />
-      <div className={styles.contentList}>
-        <div className={styles.contentList}>
+      <div>
+        <span style={{ fontSize: "20px" }}>오늘의 발견</span>
+        <span style={{ fontSize: "13px" }}>
+          | 오늘 쿠팡이 엄선한 가장 HOT한 신상품 !
+        </span>
+        <br />
+        <div className={styles.itemList}>
           {items.map((item) => (
             <ContentNewItem key={item.id} item={item}></ContentNewItem>
           ))}
         </div>
       </div>
 
-      {/* 추천상품으로 분리 */}
-      <div>
+      <div style={{ marginTop: "20px" }}>
         <span style={{ fontSize: "20px" }}>HOT! TREND</span>
         <br />
         <span style={{ fontSize: "20px" }}>카테고리별 추천상품</span>
         <br />
-
-        <div className={styles.content}>
-          <div className={styles.image}>
-            <img src="images/1.jpg" alt="shampoo" />
-          </div>
-          <div className={styles.name}>두피케어 샴푸</div>
-          <div className={styles.price}>11,340원</div>
+        <div className={styles.recoItemList}>
+          {recommendItems.map((recoItem) => (
+            <ContentRecoItem
+              key={recoItem.id}
+              item={recoItem}
+            ></ContentRecoItem>
+          ))}
         </div>
       </div>
     </div>
---
+++ shopping/src/components/ContentRecoItem.js
@@ -0,0 +1,17 @@
+import styles from "./ContentRecoItem.module.scss";
+
+const ContentRecoItem = ({ item }) => {
+  return (
+    <div>
+      <div className={styles.content}>
+        <div className={styles.image}>
+          <img src={item.src} alt="vaccum" />
+        </div>
+        <div className={styles.name}>{item.name}</div>
+        <div className={styles.price}>{item.price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ContentRecoItem;
