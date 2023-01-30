994196c8b1b236ab6f97d70db913c3b6eab69b12
ghenmaru <rbgks1937@naver.com>
2023-01-30 16:51:06 +0900

    feat: dropdown 카테고리 클릭 시 value 변경

--- shopping/src/components/common/navbar/Dropdown/Dropdown.js
+++ shopping/src/components/common/navbar/Dropdown/Dropdown.js
@@ -1,14 +1,17 @@
-import { AiFillCaretDown } from "react-icons/ai";
+import { AiFillCaretUp, AiFillCaretDown } from "react-icons/ai";
 import styles from "./Dropdown.module.scss";
 import { useState, useEffect } from "react";
 import DropdownList from "./DropdownList";
-import cn from "classnames";
 
 const Dropdown = () => {
   const [value, setValue] = useState("전체");
   const [visible, setVisible] = useState(false);
   const [visibilityAnimation, setVisibilityAnimation] = useState(false);
 
+  const onClickDropdown = (newValue) => {
+    setValue(newValue);
+    setVisible(!visible);
+  };
   const onClick = (e) => {
     setVisible(!visible);
   };
@@ -27,9 +30,13 @@ const Dropdown = () => {
     <div>
       <div className={styles.dropdown} onClick={onClick}>
         <span>{value}</span>
-        <AiFillCaretDown />
+        {visible ? <AiFillCaretUp /> : <AiFillCaretDown />}
       </div>
-      {visibilityAnimation ? <DropdownList visible={visible} /> : ""}
+      {visibilityAnimation ? (
+        <DropdownList visible={visible} onClick={onClickDropdown} />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- shopping/src/components/common/navbar/Dropdown/DropdownList.js
+++ shopping/src/components/common/navbar/Dropdown/DropdownList.js
@@ -1,8 +1,17 @@
 import styles from "./DropdownList.module.scss";
 import cn from "classnames";
-import { useState, useEffect } from "react";
 
-const DropdownList = ({ visible }) => {
+const dropdownList = [
+  "전체",
+  "여성패션",
+  "남성패션",
+  "남녀 공용 의류",
+  "뷰티",
+  "출산/유아동",
+  "식품",
+];
+
+const DropdownList = ({ visible, onClick }) => {
   return (
     <div
       className={cn(
@@ -10,14 +19,13 @@ const DropdownList = ({ visible }) => {
         visible ? styles.fadeIn : styles.fadeOut
       )}
     >
-      <div>전체</div>
-      <div>여성패션</div>
-      <div>남성패션</div>
-      <div>남녀 공용 의류</div>
-      <div>유아동패션</div>
-      <div>뷰티</div>
-      <div>출산/유아동</div>
-      <div>식품</div>
+      <div className={styles.dropdownItem}>
+        {dropdownList.map((item, key) => (
+          <div key={key} onClick={() => onClick(item)}>
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
