537417013491938a52fc98ef1972bc2126c5828a
ghenmaru <rbgks1937@naver.com>
2023-02-15 14:58:56 +0900

    style: 코드 포맷 변경

--- shopping/src/components/content/Content.js
+++ shopping/src/components/content/Content.js
@@ -1,4 +1,3 @@
-import styles from "./Content.module.scss";
 import { useState, useRef, useEffect } from "react";
 import ContentRecommendList from "./ContentRecommend/ContentRecommendList";
 import ContentNewList from "./ContentNew/ContentNewList";
@@ -14,15 +13,13 @@ const Content = () => {
   const [recommendContents, setRecommendContents] = useRecoilState(
     recoilRecommendContentList
   );
-
   const [isLoaded, setIsLoaded] = useState(false);
   const nextId = useRef(recommendContents.length + 1);
 
   // 로딩on -> 1.5초 대기(유사 비동기 처리) -> item key 처리 -> state 추가 -> 로딩off
-  const getMoreItem = async () => {
+  const getMoreContents = async () => {
     setIsLoaded(true);
-    // DB 구축 시 api 호출
-    await new Promise((resolve) => setTimeout(resolve, 1500));
+    await new Promise((resolve) => setTimeout(resolve, 1500)); // DB 구축 시 api 호출
     const items = recommendContents.map((item) => {
       return { ...item, id: nextId.current++ };
     });
@@ -33,19 +30,18 @@ const Content = () => {
     setIsLoaded(false);
   };
 
-  //
   const onIntersect = async (entries, observer) => {
     const target = entries[0];
     if (target.isIntersecting && !isLoaded) {
-      await getMoreItem();
+      await getMoreContents();
     }
   };
 
   const intersectRef = useRef(null);
 
   const options = {
-    root: null, //기본 null, 관찰대상의 부모요소를 지정
-    threshold: 0.7, // 관찰요소와 얼만큼 겹쳤을 때 콜백을 수행하도록 지정하는 요소
+    root: null,
+    threshold: 0.7,
   };
 
   // intersectionOberserver 객체 생성 -> target element 화면에 70% 보이면 onIntersect 콜백함수 호출 -> cleanner
