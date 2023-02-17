ad5b25d9cdb49400d02b303cb43abd39cf851eb4
ghenmaru <rbgks1937@naver.com>
2023-02-17 20:41:46 +0900

    design: mypage 디자인 수정

--- shopping/src/components/common/navbar/MyPage.js
+++ shopping/src/components/common/navbar/MyPage.js
@@ -5,12 +5,16 @@ const MyPage = () => {
   const router = useRouter();
   return (
     <div className={styles.myPage}>
-      <div>아이디 : ToYou</div>
+      <div className={styles.userId}>
+        <div>아이디 : </div>
+        <div style={{ marginLeft: "180px" }}>ToYou</div>
+      </div>
       <div className={styles.currentPassword}>
-        현재 비밀번호 : <input placeholder="현재 비밀번호를 입력하세요"></input>
+        <div>현재 비밀번호 : </div>
+        <input placeholder="현재 비밀번호를 입력하세요"></input>
       </div>
       <div className={styles.nextPassword}>
-        비밀번호 변경 :{" "}
+        <div>비밀번호 변경 : </div>
         <input placeholder="변경할 비밀번호를 입력하세요"></input>
       </div>
       <button onClick={() => router.push("/")}>수정하기</button>
