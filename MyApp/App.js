0ea09921cce0d1925f7fc0078f80f75da05c8e59
ghenmaru <rbgks1937@naver.com>
2023-03-14 01:30:15 +0900

    feat: flex size로 비율 조절하기

--- MyApp/App.js
+++ MyApp/App.js
@@ -3,21 +3,23 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Helloooooo!</Text>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "red" }}></View>
+      <View style={{ flex: 3, backgroundColor: "yellow" }}></View>
+      <View style={{ flex: 1, backgroundColor: "blue" }}></View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 50,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   text: {
+//     fontSize: 50,
+//   },
+// });
