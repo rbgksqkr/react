c44e90d639e2268290bcb87d4a07d559fd7ad10a
ghenmaru <rbgks1937@naver.com>
2023-03-17 00:48:07 +0900

    feat: 음성 녹음 및 재생 기능 구현

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,80 +1,25 @@
 import { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { Audio } from "expo-av";
-
+import AudioPlayer from "./src/AudioPlayer";
+import AudioRecorder from "./src/AudioRecorder";
 export default function App() {
-  const [recording, setRecording] = useState();
-  const [sound, setSound] = useState();
-
-  async function startRecording() {
-    try {
-      console.log("Requesting permissions..");
-      await Audio.requestPermissionsAsync();
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-      });
-
-      console.log("Starting recording..");
-      const { recording } = await Audio.Recording.createAsync(
-        Audio.RecordingOptionsPresets.HIGH_QUALITY
-      );
-      setRecording(recording);
-      console.log("Recording started");
-    } catch (err) {
-      console.error("Failed to start recording", err);
-    }
-  }
-
-  async function stopRecording() {
-    console.log("Stopping recording..");
-    setRecording(undefined);
-    await recording.stopAndUnloadAsync();
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-    });
-    const uri = recording.getURI();
-    console.log("Recording stopped and stored at", uri);
-  }
-
-  async function playSound() {
-    console.log("Loading Sound");
-    const { sound } = await Audio.Sound.createAsync(
-      require("./assets/Hello.mp3")
-    );
-    setSound(sound);
-
-    console.log("Playing Sound");
-    await sound.playAsync();
-  }
-
-  useEffect(() => {
-    return sound
-      ? () => {
-          console.log("Unloading Sound");
-          sound.unloadAsync();
-        }
-      : undefined;
-  }, [sound]);
   return (
     <View style={styles.container}>
-      <Text>ads</Text>
-
-      <Button title={"!!!"}></Button>
-      <Button
-        color="black"
-        title={recording ? "Stop Recording" : "Start Recording"}
-        onPress={recording ? stopRecording : startRecording}
-      />
-      <Button title="Play Sound" onPress={playSound} />
+      <View>
+        <AudioRecorder />
+      </View>
+      <View>
+        <AudioPlayer />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    justifyContent: "center",
+    flex: 1,
     alignItems: "center",
+    justifyContent: "center",
   },
 });
