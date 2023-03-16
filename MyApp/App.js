83741d0d0e8348939019ee614e492219847a85ed
ghenmaru <rbgks1937@naver.com>
2023-03-16 23:57:14 +0900

    feat: 음성 녹음 기능

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,110 +1,80 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Dimensions,
-  ActivityIndicator,
-} from "react-native";
-import * as Location from "expo-location";
-import { useEffect } from "react";
-import { useState } from "react";
-import { config } from "./config";
-
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
-const API_KEY = config.API_KEY;
+import { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { Audio } from "expo-av";
 
 export default function App() {
-  const [ok, setOk] = useState(true);
-  const [city, setCity] = useState("...loading");
-  const [days, setDays] = useState([]);
-  const getWeather = async () => {
-    const { granted } = await Location.requestForegroundPermissionsAsync();
-    if (!granted) {
-      setOk(false);
+  const [recording, setRecording] = useState();
+  const [sound, setSound] = useState();
+
+  async function startRecording() {
+    try {
+      console.log("Requesting permissions..");
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log("Starting recording..");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
     }
+  }
 
-    const {
-      coords: { latitude, longitude },
-    } = await Location.getCurrentPositionAsync();
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-    );
-    const json = await response.json();
-    const list = json["list"].slice(0, 5);
-    setDays(list);
+  async function stopRecording() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording.getURI();
+    console.log("Recording stopped and stored at", uri);
+  }
 
-    const location = await Location.reverseGeocodeAsync(
-      { latitude, longitude },
-      { useGoogleMaps: false }
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/Hello.mp3")
     );
-    setCity(location[0].city);
-  };
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
   useEffect(() => {
-    getWeather();
-  }, []);
-
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
   return (
     <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>{city}</Text>
-      </View>
-      <ScrollView
-        pagingEnabled
-        horizontal
-        contentContainerStyle={styles.weather}
-        indicatorStyle="white"
-      >
-        {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator
-              color="white"
-              style={{ marginTop: 60 }}
-              size="large"
-            />
-          </View>
-        ) : (
-          days.map((day, index) => (
-            <View style={styles.day} key={index}>
-              <Text style={styles.temp}>{day.main.temp}</Text>
-              <Text style={styles.description}>{day.weather[0].main}</Text>
-              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
-            </View>
-          ))
-        )}
-      </ScrollView>
+      <Text>ads</Text>
+
+      <Button title={"!!!"}></Button>
+      <Button
+        color="black"
+        title={recording ? "Stop Recording" : "Start Recording"}
+        onPress={recording ? stopRecording : startRecording}
+      />
+      <Button title="Play Sound" onPress={playSound} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "tomato",
-  },
-  city: {
-    flex: 1,
-    alignItems: "center",
+    // flex: 1,
     justifyContent: "center",
-  },
-  cityName: {
-    fontSize: 50,
-  },
-  weather: {
-    backgroundColor: "tomato",
-  },
-  day: {
     alignItems: "center",
-    width: SCREEN_WIDTH,
-  },
-  temp: {
-    fontSize: 160,
-    marginTop: 70,
-  },
-  description: {
-    fontSize: 50,
-  },
-  tinyText: {
-    fontSize: 20,
   },
 });
